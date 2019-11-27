631448413735c0078c2953ff5722e07df1506b32
Daniela <lemmy5738@gmail.com>
2019-11-27 01:28:15 +0100

    ...

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php $thisPage="Startseite"; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Sandbox</title>
+
+    
+</head>
+<link rel="stylesheet" href="main.css">
+<script src="zeit.js"></script>
+
+<body>
+
+
+<main>
+	<div id="uhr"></div>
+</main>
+    
+</body>
+
+</html>
